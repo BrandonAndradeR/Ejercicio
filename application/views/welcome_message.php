b50b55d05749003f8f0fa78a90878d6bb8c23aba
BrandonAndradeR <60976033+BrandonAndradeR@users.noreply.github.com>
2020-02-20 20:56:55 -0500

    Agregar
    Agregue todo lo que tenia de la primera carpeta que tenia empezada en clases

---
+++ application/views/welcome_message.php
@@ -0,0 +1,7 @@
+Escriba Un Impar del 1 al 7
+<?php //echo base_url(); ?>
+<form action="<?php echo base_url(); ?>index.php/Welcome/Other" 
+method="POST">
+<input type="text" name="NumImpar" id="">
+<button type="submit">Enviar</button>
+</form>
